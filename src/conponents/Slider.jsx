18ba6601308ca58048716345475de3ef8914becc
mahadiduet <mahadiduet@gmail.com>
2024-09-24 16:53:00 +0600

    banner responsive done

--- src/conponents/Slider.jsx
+++ src/conponents/Slider.jsx
@@ -1,10 +1,10 @@
 
 const Slider = () => {
     return (
-        <div className="carousel w-full">
+        <div className="carousel w-full h-[350px]">
             <div id="slide1" className="carousel-item relative w-full">
                 <img
-                    src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
+                    src="https://i.ibb.co.com/wd6qbZn/Slider1.jpg"
                     className="w-full" />
                 <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
                     <a href="#slide4" className="btn btn-circle">❮</a>
@@ -13,7 +13,7 @@ const Slider = () => {
             </div>
             <div id="slide2" className="carousel-item relative w-full">
                 <img
-                    src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
+                    src="https://i.ibb.co.com/sCgWcCc/slider2.jpg"
                     className="w-full" />
                 <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
                     <a href="#slide1" className="btn btn-circle">❮</a>
@@ -22,7 +22,7 @@ const Slider = () => {
             </div>
             <div id="slide3" className="carousel-item relative w-full">
                 <img
-                    src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
+                    src="https://i.ibb.co.com/XXcRFxn/slider3.jpg"
                     className="w-full" />
                 <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
                     <a href="#slide2" className="btn btn-circle">❮</a>
@@ -31,7 +31,7 @@ const Slider = () => {
             </div>
             <div id="slide4" className="carousel-item relative w-full">
                 <img
-                    src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
+                    src="https://i.ibb.co.com/TqSM6XF/slider4.jpg"
                     className="w-full" />
                 <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
                     <a href="#slide3" className="btn btn-circle">❮</a>

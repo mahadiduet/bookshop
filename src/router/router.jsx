3835beec4f5c35a581ced9a30290551e78344e9c
mahadiduet <mahadiduet@gmail.com>
2024-09-24 16:19:01 +0600

    error route set

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import Homepage from "../page/homepage/Homepage";
 import Faqpage from "../page/faq/Faqpage";
 import Bookpage from "../page/books/bookpage";
+import Errorpage from "../page/error/Errorpage";
 
 export const router = createBrowserRouter([
     {
         path: "/",
         element: <Homepage />,
+        errorElement: <Errorpage />
     },
     {
         path: `/book/:id`,
